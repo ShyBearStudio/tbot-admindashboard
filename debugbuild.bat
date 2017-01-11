559412b42262a3bd450899fb2a60f5e5ef4e6129
Vadim Malakhov <malakhovadim@gmail.com>
2017-01-10 23:02:13 -0800

    Fix debugbuild script

--- debugbuild.bat
+++ debugbuild.bat
@@ -1,6 +1,7 @@
 godebug build -instrument ^
-github.com/ShyBearStudio/tbot-admindashboard/data,^
+github.com/ShyBearStudio/tbot-admindashboard/models,^
 github.com/ShyBearStudio/tbot-admindashboard/configutils,^
+github.com/ShyBearStudio/tbot-admindashboard/dbutils,^
 github.com/ShyBearStudio/tbot-admindashboard/logger,^
 github.com/ShyBearStudio/tbot-admindashboard/projects/echobot,^
 github.com/ShyBearStudio/tbot-admindashboard/projects/echobot/data
